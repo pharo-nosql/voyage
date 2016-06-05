f0a19e5f1b4ca6d3b5bbc69180265eeba795d78d
tinchodias <tinchodias@gmail.com>
2016-06-05 00:24:45 -0300

    display something in main scripts

--- scripts/rs-serve.sh
+++ scripts/rs-serve.sh
@@ -9,6 +9,7 @@ $DIR/rs-checkEnvirnomentVariables.sh
 # serve dbs
 for i in `seq 1 3`;
 do
+	echo starting mongod at port 2703$i
 	mongod --port 2703$i --dbpath $baseRepositoryPath$replicaSetName$i --replSet $replicaSetName --smallfiles --oplogSize 128 &>/dev/null &
 done
 
--- scripts/rs-stopAll.sh
+++ scripts/rs-stopAll.sh
@@ -2,6 +2,7 @@
 
 for i in `seq 1 3`;
 do
+	echo stop mongo at port 2703$i
 	mongo --port 2703$i --eval "db.getSiblingDB('admin').shutdownServer()"
 done
 
