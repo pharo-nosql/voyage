308f81dc54dd8b91d92844b26747ae977e84af93
Norbert Hartl <norbert@hartl.name>
2019-06-24 16:31:54 +0200

    disable auth

--- .install_arangodb.sh
+++ .install_arangodb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 docker pull arangodb:3.4
-docker run -d -e ARANGO_ROOT_PASSWORD="test" -p 8529:8529 arangodb:3.4
+docker run -d -e ARANGO_NO_AUTH=1 -p 8529:8529 arangodb:3.4 
 
 sleep 2
 
 n=0
 # timeout value for startup
 timeout=60 
-while [[ (-z `curl -H 'Authorization: Basic cm9vdDp0ZXN0' -s 'http://127.0.0.1:8529/_api/version' `) && (n -lt timeout) ]] ; do
+while [[ (-z `curl -s 'http://127.0.0.1:8529/_api/version' `) && (n -lt timeout) ]] ; do
   echo -n "."
   sleep 1s
   n=$[$n+1]
